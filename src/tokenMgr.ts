36eeb929cd4c6ed2abd16ddfa9c740189d7451f3
tongjinle <1328192478@qq.com>
2018-01-07 14:16:34 +0800

    fix token cache

--- src/tokenMgr.ts
+++ src/tokenMgr.ts
@@ -26,9 +26,19 @@ import config from './config';
 
 export default class TokenMgr {
     private dict: { [token: string]: { openId: string, expires: number, } };
+    private opDict: { [openId: string]: string };
     private constructor() {
         this.dict = {};
-        this.dict['testToken'] = { openId: 'puman', expires: new Date(2999, 0, 1).getTime(), };
+        this.opDict = {};
+
+        // mock
+        {
+            let token = 'testToken',
+                openId = 'puman',
+                expires = new Date(2999, 0, 1).getTime();
+            this.dict[token] = { openId, expires, };
+            this.opDict[openId] = token;
+        }
     }
 
     private static ins: TokenMgr;
@@ -46,9 +56,18 @@ export default class TokenMgr {
 
     // 绑定openId,生成token
     bind(openId: string): string {
+        // 使用缓存的未过期的token
+        {
+            let token = this.opDict[openId];
+            if(this.check(token)){
+                return token;
+            }
+        }
+        // 创建token
         const duration = config.tokenExpires;
         let token: string = Math.floor((10e8 * Math.random())).toString(16).slice(0, 8);
         this.dict[token] = { openId, expires: Date.now() + duration, };
+        this.opDict[openId] = token;
         return token;
     }
 
