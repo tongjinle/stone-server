635b1944fe766f0ae1f30ab0cdd2b1a17d4aaef9
tongjinle <1328192478@qq.com>
2018-02-03 00:12:26 +0800

    ~

--- src/test/spec/checkSpec.ts
+++ src/test/spec/checkSpec.ts
@@ -60,19 +60,6 @@ let fn: ITestFunc = async function({ db, axi, }) {
             ret.push({ title: '进行check', expect: 0, calc: data.code, });
 
         }
-        // await db.removeUserAll();
-
-        // let dotaId = '1328192478';
-        // let reward = 1000;
-        // let bindTime = new Date(2000, 1, 1).getTime();
-        // let dotaId2 = '553740215';
-
-        // await axi.post(apiPrefix + 'bind', { dotaId, } as Protocol.IReqBind) as { data: Protocol.IResBind };
-
-        // let { data: { code, } } = await axi.post(apiPrefix + 'bind', { dotaId: dotaId2, } as Protocol.IReqBind) as { data: Protocol.IResBind };
-        // let { data, } = await axi.get(apiPrefix + 'auth/user/info') as { data: Protocol.IResUserInfo }
-
-        // ret.push({ title: 'bind once more && query userInfo', expect: dotaId2, calc: data ? data.dotaId : -1 });
     }
 
     return ret;
